4502ccb8bc9bf727a550973fe9eb779f8e74b8cc
Bayraa <bayraal3344@gmail.com>
2020-09-16 19:59:29 +0800

    hud zasaw

--- app.js
+++ app.js
@@ -9,20 +9,32 @@ var nameAndPass = [
   },
 ];
 
-console.log(nameAndPass[0].name);
-
 function check() {
   var User1 = document.getElementById("user").value;
   var Pass1 = document.getElementById("pass").value;
   for (var i = 0; i < nameAndPass.length; i++) {
+    console.log(nameAndPass[i]);
     if (User1 === nameAndPass[i].name && Pass1 === nameAndPass[i].pass) {
       alert("Та амжилттай нэвтэрлээ");
+      break;
+    } else if (User1 === nameAndPass[1].name && Pass1 === nameAndPass[1].pass) {
+      alert("Та амжилттай нэвтэрлээ");
+      break;
     } else if (Pass1 !== nameAndPass[i].pass && User1 === nameAndPass[i].name) {
       alert("Wrong password");
+      break;
+    } else if (Pass1 !== nameAndPass[1].pass && User1 === nameAndPass[1].name) {
+      alert("Wrong password");
+      break;
     } else if (User1 !== nameAndPass[i].name && Pass1 === nameAndPass[i].pass) {
       alert("UserName олдохгүй байна");
+      break;
+    } else if (User1 !== nameAndPass[i].name && Pass1 === nameAndPass[i].pass) {
+      alert("UserName олдохгүй байна");
+      break;
     } else if (User1 !== nameAndPass[i].name && Pass1 !== nameAndPass[i].pass) {
       alert("UserName болон password буруу байна");
+      break;
     }
   }
 }
